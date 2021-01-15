9d35d739a7cc368117c88240c535844e923c564a
monstaro <codysmith2814@gmail.com>
2021-01-15 16:44:08 -0700

    Adds classNames for flex containers and searchbar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './Header.css';
+import Searchbar from '../Searchbar/Searchbar.js';
 
 const Header = () => {
         return (
             <div className="header-container">
-                <h1 className="header-site-title">RestaurantTier</h1>
-                <h2 className="header-sub-title">Your go-to guide to urban eats</h2>
-
+                <span className="header-title-area">
+                    <h1 className="header-site-title">RestaurantTier</h1>
+                    <h2 className="header-sub-title">Your go-to guide to urban eats</h2>
+                </span>
+                <span className="header-searchbar-area">
+                    <Searchbar />
+                </span>
             </div>
         )
 }
