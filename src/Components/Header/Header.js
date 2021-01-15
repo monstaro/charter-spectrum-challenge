185b698dac25013a4a4a9c937d9ae52264d1e7d2
monstaro <codysmith2814@gmail.com>
2021-01-15 15:25:49 -0700

    Adds header and subheader text

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+        return (
+            <div className="header-container">
+                <h1 className="header-site-title">RestaurantTier</h1>
+                <h2 className="header-sub-title">Your go-to guide to urban eats</h2>
+
+            </div>
+        )
+}
+
+export default Header;
