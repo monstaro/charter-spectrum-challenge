b85634018aa87128c2bea9afc2c767a02a93deef
monstaro <codysmith2814@gmail.com>
2021-01-15 16:59:13 -0700

    Styles search area

--- src/Components/Searchbar/Searchbar.js
+++ src/Components/Searchbar/Searchbar.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Searchbar.css';
+import SearchIcon from '../../Assets/magnifying-glass.svg';
 
 const Searchbar = () => {
     return (
         <div className="searchbar-container">
             <input type="text" placeholder="Find some food" className="searchbar-input" />
+            <img className="searchbar-icon" src={SearchIcon} alt="Press to Search"/>
         </div>
     )
 }
