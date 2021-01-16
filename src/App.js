02c91ebd90d1bd5744ecd919abcb94c0cac72513
monstaro <codysmith2814@gmail.com>
2021-01-16 12:14:51 -0700

    Imports restaurants container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Components/Header/Header';
+import RestaurantsContainer from './Components/RestaurantsContainer/RestaurantsContainer'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <RestaurantsContainer />
     </div>
   );
 }
