49ebd7cd0dc508fbfd82a18e850bdd7746af0107
Cody Smith <codysmith@Codys-MacBook-Pro.local>
2021-01-16 16:31:00 -0700

    Adds table and styling

--- src/Components/Restaurant/Restaurant.js
+++ src/Components/Restaurant/Restaurant.js
@@ -5,6 +5,18 @@ const Restaurant = (props) => {
   const { name } = props;
     return (
         <div className="restaurant-card">
+        <table>
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td>
+    <td>50</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td>
+    <td>94</td>
+  </tr>
+</table>
             { name }
         </div>
     )
