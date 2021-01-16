1f489116f7f9bdc6eb7ed088380c5ab7c976ef05
Cody Smith <codysmith@Codys-MacBook-Pro.local>
2021-01-16 14:26:42 -0700

    Displays props from parent

--- src/Components/Restaurant/Restaurant.js
+++ src/Components/Restaurant/Restaurant.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Restaurant.css';
 
-const Restaurant = () => {
+const Restaurant = (props) => {
+  const { name } = props;
     return (
-        <div>
-            Restaurant
+        <div className="restaurant-card">
+            { name }
         </div>
     )
 }
