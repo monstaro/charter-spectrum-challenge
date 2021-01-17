f1fa9dca9d9b790b990e34856f92b7382061ffd7
Cody Smith <codysmith@Codys-MacBook-Pro.local>
2021-01-17 16:04:48 -0700

    Turn to stateful component to pass down data and recieve actions

--- src/App.js
+++ src/App.js
@@ -4,17 +4,39 @@ import Filter from './Components/Filter/Filter';
 import RestaurantsContainer from './Components/RestaurantsContainer/RestaurantsContainer';
 import {RestaurantProvider} from './RestaurantContext';
 import {allStates} from './utilities.js'
-function App() {
+import React, { Component } from 'react'
+import getRestaurants from './apiCalls.js';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      allRestaurants: [],
+      selectedRestaurants: []
+    }
+  }
+
+  componentDidMount() {
+    getRestaurants()
+    .then(data => this.setState({
+      allRestaurants: data.sort((a, b) => a.name.localeCompare(b.name))
+    }))
+    }
+
+render() {
   return (
     <RestaurantProvider>
     <div className="App">
       <Header />
       <Filter type="state" options={allStates}/>
       <Filter type="genre"/>
-      <RestaurantsContainer />
+      <RestaurantsContainer allRestaurants={this.state.allRestaurants} selectedRestaurants={this.state.selectedRestaurants}/>
     </div>
     </RestaurantProvider>
   );
+}
+
 }
 
 export default App;
