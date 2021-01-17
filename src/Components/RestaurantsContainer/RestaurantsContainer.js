6785dee7bc8d3f735e5590129b7e201206188f6c
Cody Smith <codysmith@Codys-MacBook-Pro.local>
2021-01-17 16:05:20 -0700

    Removes context

--- src/Components/RestaurantsContainer/RestaurantsContainer.js
+++ src/Components/RestaurantsContainer/RestaurantsContainer.js
@@ -1,46 +1,50 @@
-import React, { useContext, useEffect } from 'react';
+import React, { Component } from 'react';
 import './RestaurantsContainer.scss';
-import {RestaurantContext} from '../../RestaurantContext';
-import getRestaurants from '../../apiCalls.js';
+// import {RestaurantContext} from '../../RestaurantContext';
+// import getRestaurants from '../../apiCalls.js';
+// import Restaurant from './Restaurant/Restaurant';
 
-const RestaurantsContainer = () => {
-  const [restaurants, setRestaurants] = useContext(RestaurantContext);
+const RestaurantsContainer = (props) => {
+  const { allRestaurants } = props
+  console.log(allRestaurants)
+// componentDidMount() {
+//   getRestaurants()
+//   .then(data => this.setState({
+//     restaurants: data.sort((a, b) => a.name.localeCompare(b.name))
+//   }))
+//   }
 
-  useEffect(() => {
-     getRestaurants()
-    .then(data => setRestaurants(data.sort((a, b) => a.name.localeCompare(b.name))))
-  });
 
-     if (!restaurants.length) {
-       return (<div className="restaurants-container">Please wait...</div>)
-     } else {
-       return (
-      <div className="restaurants-container">
-      <table className="restaurants-table">
-        <thead>
-        <tr>
-          <th>Name</th>
-          <th>City</th>
-          <th>State</th>
-          <th>Phone</th>
-          <th>Category</th>
-          </tr>
-        </thead>
-        {restaurants.map(restaurant =>
-          <tbody>
+       if (!allRestaurants.length) {
+         return (<div className="restaurants-container">Please wait...</div>)
+       } else {
+         return (
+        <div className="restaurants-container">
+        <table className="restaurants-table">
+          <thead>
           <tr>
-          <td>{restaurant.name}</td>
-          <td>{restaurant.city}</td>
-          <td>{restaurant.state}</td>
-          <td>{restaurant.telephone}</td>
-          <td>{restaurant.genre}</td>
-          </tr>
-          </tbody>
-        )}
-      </table>
-      </div>
-    )
-   }
-}
+            <th>Name</th>
+            <th>City</th>
+            <th>State</th>
+            <th>Phone</th>
+            <th>Category</th>
+            </tr>
+          </thead>
+          {allRestaurants.map(restaurant =>
+            <tbody>
+            <tr>
+            <td>{restaurant.name}</td>
+            <td>{restaurant.city}</td>
+            <td>{restaurant.state}</td>
+            <td>{restaurant.telephone}</td>
+            <td>{restaurant.genre}</td>
+            </tr>
+            </tbody>
+          )}
+        </table>
+        </div>
+      )
+    }
+  }
 
 export default RestaurantsContainer;
