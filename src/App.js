13d6e01ee85e72189878642b66b386cefac58e3b
Cody Smith <codysmith@Codys-MacBook-Pro.local>
2021-01-16 19:06:59 -0700

    Adds in filter components and imports states

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Header from './Components/Header/Header';
+import Filter from './Components/Filter/Filter';
 import RestaurantsContainer from './Components/RestaurantsContainer/RestaurantsContainer';
 import {RestaurantProvider} from './RestaurantContext';
-
+import {allStates} from './utilities.js'
 function App() {
+  console.log(allStates)
+
   return (
     <RestaurantProvider>
     <div className="App">
       <Header />
+      <Filter type="state" options={allStates}/>
+      <Filter type="genre"/>
       <RestaurantsContainer />
     </div>
     </RestaurantProvider>
