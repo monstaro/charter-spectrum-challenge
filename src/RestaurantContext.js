2c67e50cfc3fdec270095f5910a07c911a9dfdcb
Cody Smith <codysmith@Codys-MacBook-Pro.local>
2021-01-16 19:48:17 -0700

    removes whitespace

--- src/RestaurantContext.js
+++ src/RestaurantContext.js
@@ -6,7 +6,6 @@ export const RestaurantContext = createContext();
 export const RestaurantProvider = props => {
   const [restaurants, setRestaurants] = useState([])
 
-
   return (
     <RestaurantContext.Provider value={[restaurants, setRestaurants]}>
       {props.children}
