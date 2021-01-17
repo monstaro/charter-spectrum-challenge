8c4b096d6c87d00b63252d37266f54517a1bad00
Cody Smith <codysmith@Codys-MacBook-Pro.local>
2021-01-16 18:01:59 -0700

    Brings in context provider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Header from './Components/Header/Header';
-import RestaurantsContainer from './Components/RestaurantsContainer/RestaurantsContainer'
+import RestaurantsContainer from './Components/RestaurantsContainer/RestaurantsContainer';
+import {RestaurantProvider} from './RestaurantContext';
 
 function App() {
   return (
+    <RestaurantProvider>
     <div className="App">
       <Header />
       <RestaurantsContainer />
     </div>
+    </RestaurantProvider>
   );
 }
 
