dd7c7894832a564040f04e69d991406eb8052cd6
Cody Smith <codysmith@Codys-MacBook-Pro.local>
2021-01-17 19:55:34 -0700

    Adds search function to b utton

--- src/Components/Searchbar/Searchbar.js
+++ src/Components/Searchbar/Searchbar.js
@@ -12,7 +12,7 @@ const Searchbar = (props) => {
     return (
         <form className="searchbar-container">
             <input type="text" placeholder="Find some food" className="searchbar-input" onKeyPress={(e) => handleKeyDown(e)}/>
-            <img className="searchbar-icon" src={SearchIcon} alt="Press to Search"/>
+            <img className="searchbar-icon" src={SearchIcon} alt="Press to Search" onClick={(e) => handleKeyDown(e)}/>
         </form>
     )
 }
