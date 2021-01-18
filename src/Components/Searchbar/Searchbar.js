bc69ef3b82d7cbac067d18d5fc38d43cd5af487f
Cody Smith <codysmith@Codys-MacBook-Pro.local>
2021-01-18 12:39:32 -0700

    Search works on button press

--- src/Components/Searchbar/Searchbar.js
+++ src/Components/Searchbar/Searchbar.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Searchbar.css';
 import SearchIcon from '../../Assets/magnifying-glass.svg';
 
 const Searchbar = (props) => {
+  const [searchTerm, setSearchTerm] = useState('')
   const handleKeyDown = (e) => {
       if (e.key === 'Enter') {
         e.preventDefault();
         props.searchHandler(e.target.value.split(' '))
+      }
+      if (e.type === 'click') {
+        props.searchHandler(searchTerm)
       }
   }
 
@@ -17,7 +21,7 @@ const Searchbar = (props) => {
   }
     return (
         <form className="searchbar-container">
-            <input type="text" placeholder="Find some food" className="searchbar-input" onKeyPress={(e) => handleKeyDown(e)} onKeyUp={(e) => handleDelete(e)}/>
+            <input type="text" placeholder="Find some food" className="searchbar-input" onChange={(e) => setSearchTerm(e.target.value.split(' '))} onKeyPress={(e) => handleKeyDown(e)} onKeyUp={(e) => handleDelete(e)}/>
             <img className="searchbar-icon" src={SearchIcon} alt="Press to Search" onClick={(e) => handleKeyDown(e)}/>
         </form>
     )
